8e790f95d69e07831efe277333fad7232050316d
b3nj1r <47711426+b3nj1r@users.noreply.github.com>
2021-01-22 10:18:50 -0800

    implemented basic circle render

--- src/graphics/main.cpp
+++ src/graphics/main.cpp
@@ -1,21 +1,40 @@
+#define _USE_MATH_DEFINES
 #include <GL/glut.h>
 #include <math.h>
-
-void drawCircle()
+#include <stdio.h>
+const float pi = 3.14159;
+void drawCircle(float a, float b, float r, int i)
 {
-	
+	glBegin(GL_TRIANGLE_FAN);
+	// iterate from E[0, 2pi] 
+	float inc = pi/i;
+	for(float t = 0; t < 2*pi; t+=inc)
+	{
+		// parametric definition of a circle
+		// x = a + r cos t
+		// y = b + r sin t
+		float x = a + r * cos(t);
+		float y = b + r * sin(t);
+		glVertex2f(x,y);
+	}
+	glEnd();
 }
+// initialization function
 void start()
 {
-	glClearColor(1.0,1.0,1.0,1.0);
+	glClearColor(0.0,0.0,0.0,0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
+// frame based continous update
 void update()
 {
-	
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0,1.0,1.0);
+	drawCircle(0.5, 0.5, 0.2, 4);
+	glFlush();
 }
 
 int main(int argc, char** argv)
@@ -27,8 +46,6 @@ int main(int argc, char** argv)
 	glutCreateWindow("2d");
 	start();
 	glutDisplayFunc(update);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0,1.0,1.0);
 	glutMainLoop();
 	return 0;
 }
